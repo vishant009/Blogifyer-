c767af866e1f946ebbd685365cc89ed23386843d
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 17:34:42 +0530

    Update notification.js

--- models/notification.js
+++ models/notification.js
@@ -1,23 +1,16 @@
-const { Schema, model } = require("mongoose");
+// models/notification.js
+const { Schema, model } = require('mongoose');
 
 const notificationSchema = new Schema(
   {
-    recipient: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    type: {
-      type: String,
-      enum: ["FOLLOW_REQUEST", "LIKE"],
-      required: true,
-    },
-    blogId: { type: Schema.Types.ObjectId, ref: "Blog", default: null },
-    status: {
-      type: String,
-      enum: ["PENDING", "ACCEPTED", "REJECTED", "READ"],
-      default: "PENDING",
-    },
-    message: { type: String, required: true },
+    recipient: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    type: { type: String, enum: ['FOLLOW_REQUEST', 'LIKE'], required: true },
+    blogId: { type: Schema.Types.ObjectId, ref: 'Blog', default: null },
+    status: { type: String, enum: ['PENDING', 'ACCEPTED', 'REJECTED', 'READ'], default: 'PENDING' },
+    message: { type: String, required: true }
   },
   { timestamps: true }
 );
 
-module.exports = model("Notification", notificationSchema);
+module.exports = model('Notification', notificationSchema);
