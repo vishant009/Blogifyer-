afe036329f7be420c079aa732cc5f830128810e2
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 17:34:17 +0530

    Update comments.js

--- models/comments.js
+++ models/comments.js
@@ -1,24 +1,23 @@
-const { Schema, model } = require("mongoose");
+// models/comments.js
+const { Schema, model } = require('mongoose');
 
 const commentSchema = new Schema(
   {
     content: { type: String, required: true },
-    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
-    blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
-    parentCommentId: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
-    replies: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    blogId: { type: Schema.Types.ObjectId, ref: 'Blog' },
+    parentCommentId: { type: Schema.Types.ObjectId, ref: 'Comment', default: null },
+    replies: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
   },
   { timestamps: true }
 );
 
-commentSchema.pre("save", async function (next) {
+commentSchema.pre('save', async function (next) {
   if (this.parentCommentId) {
-    await Comment.findByIdAndUpdate(this.parentCommentId, {
-      $addToSet: { replies: this._id },
-    });
+    await Comment.findByIdAndUpdate(this.parentCommentId, { $addToSet: { replies: this._id } });
   }
   next();
 });
 
-module.exports = model("Comment", commentSchema);
+module.exports = model('Comment', commentSchema);
