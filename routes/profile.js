548485309db91105b9512a95bbe3cae1547b9f09
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 22:01:14 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,5 @@
-// routes/profile.js
 const { Router } = require('express');
-const { randomBytes } = require('crypto');
+const { randomBytes, createHmac } = require('crypto'); // ✅ ADDED
 const User = require('../models/user');
 const Blog = require('../models/blog');
 const cloudinaryUpload = require('../middlewares/cloudinaryUpload');
