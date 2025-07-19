966919672035266739c6f36cd1ab69802d546605
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 14:30:19 +0530

    Update comments.js

--- models/comments.js
+++ models/comments.js
@@ -5,9 +5,20 @@ const commentSchema = new Schema(
     content: { type: String, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: "User" },
     blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
+    parentCommentId: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
+    replies: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
+
+commentSchema.pre("save", async function (next) {
+  if (this.parentCommentId) {
+    await Comment.findByIdAndUpdate(this.parentCommentId, {
+      $addToSet: { replies: this._id },
+    });
+  }
+  next();
+});
 
 module.exports = model("Comment", commentSchema);
