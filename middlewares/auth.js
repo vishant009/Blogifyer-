02e9bb3c0142be9f7584e3c0787820303b847acb
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 17:38:13 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,43 @@
-const { validateToken } = require("../services/authentication");
+// middlewares/auth.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-function checkForAuthenticationCookie(cookieName) {
-  return (req, res, next) => {
-    const tokenCookieValue = req.cookies[cookieName];
-    if (!tokenCookieValue) {
+const checkForAuthenticationCookie = (cookieName) => {
+  return async (req, res, next) => {
+    const token = req.cookies[cookieName];
+    if (!token) {
+      req.user = null;
       return next();
     }
-
     try {
-      const userPayload = validateToken(tokenCookieValue);
-      req.user = userPayload;
-    } catch (error) {}
-    return next();
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.userId).select('fullname email role profileImageURL followers following');
+      req.user = user;
+      next();
+    } catch (err) {
+      console.error('JWT verification error:', err);
+      req.user = null;
+      next();
+    }
   };
-}
+};
+
+const ensureAuthenticated = (req, res, next) => {
+  if (!req.user) {
+    return res.redirect('/user/signin?error_msg=Please log in to access this page');
+  }
+  next();
+};
+
+const ensureNotAuthenticated = (req, res, next) => {
+  if (req.user) {
+    return res.redirect('/?error_msg=You are already logged in');
+  }
+  next();
+};
 
 module.exports = {
   checkForAuthenticationCookie,
+  ensureAuthenticated,
+  ensureNotAuthenticated
 };
