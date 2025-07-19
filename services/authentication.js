1e0450fdaf90c80bbdeeb00a1dccdcb8b5c9847b
vishant009 <webdeveloper10980@gmail.com>
2025-07-19 22:34:20 +0530

    Update authentication.js

--- services/authentication.js
+++ services/authentication.js
@@ -1,13 +1,13 @@
-// services/authentication.js
 const jwt = require('jsonwebtoken');
 
-const createTokenForUser = (user) => {
+function createTokenForUser(user) {
   const payload = {
-    userId: user._id,
+    _id: user._id,
     email: user.email,
+    fullname: user.fullname,
     role: user.role
   };
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
-};
+}
 
 module.exports = { createTokenForUser };
