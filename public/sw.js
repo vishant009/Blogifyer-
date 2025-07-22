b0bb37542f7880265df09390ebef114f645a2037
vishant009 <webdeveloper10980@gmail.com>
2025-07-22 11:33:19 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,32 @@
 // public/sw.js
-self.addEventListener('push', event => {
-  const { title, body, url } = event.data.json();
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  const { title, body, url } = data;
+
   event.waitUntil(
     self.registration.showNotification(title, {
       body,
-      icon: '/default-profile.png',
-      data: { url }
+      icon: '/images/default.png',
+      badge: '/images/default.png',
+      data: { url },
+      requireInteraction: true,
     })
   );
 });
 
-self.addEventListener('notificationclick', event => {
+self.addEventListener('notificationclick', (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow(event.notification.data.url));
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      const url = event.notification.data.url || '/';
+      for (const client of clientList) {
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
