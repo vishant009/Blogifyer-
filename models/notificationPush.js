45be51f210f79194daad498f62a374b8a66c5567
vishant009 <webdeveloper10980@gmail.com>
2025-07-22 11:10:11 +0530

    Update notificationPush.js

--- models/notificationPush.js
+++ models/notificationPush.js
@@ -1,21 +1,18 @@
+// models/notificationPush.js
 const { Schema, model } = require("mongoose");
 
-const pushNotificationSchema = new Schema(
+const pushSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true, unique: true },
-    subscription: {
-      type: {
-        endpoint: { type: String, required: true },
-        expirationTime: { type: Date, default: null },
-        keys: {
-          p256dh: { type: String, required: true },
-          auth: { type: String, required: true },
-        },
-      },
-      required: true,
-    },
+    userId:      { type: Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+    subscription:{           // Web-push subscription object
+      endpoint:  { type: String, required: true },
+      keys: {
+        p256dh:  { type: String, required: true },
+        auth:    { type: String, required: true }
+      }
+    }
   },
   { timestamps: true }
 );
 
-module.exports = model("PushNotification", pushNotificationSchema);
+module.exports = model("PushNotification", pushSchema);
