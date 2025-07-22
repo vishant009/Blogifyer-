b8b1c6e67cb03c47ce1b28eb512ab7fbebe10951
vishant009 <webdeveloper10980@gmail.com>
2025-07-22 11:32:20 +0530

    Update notification.js

--- models/notification.js
+++ models/notification.js
@@ -3,13 +3,13 @@ const { Schema, model } = require("mongoose");
 
 const notificationSchema = new Schema(
   {
-    recipient:  { type: Schema.Types.ObjectId, ref: "User", required: true },
-    sender:     { type: Schema.Types.ObjectId, ref: "User" },
-    type:       { type: String, enum: ["NEW_BLOG","LIKE","NEW_COMMENT","LIKE_COMMENT","FOLLOW_REQUEST"], required: true },
-    message:    { type: String, required: true },
-    blogId:     { type: Schema.Types.ObjectId, ref: "Blog" },
-    status:     { type: String, enum: ["PENDING","READ"], default: "PENDING" },
-    isRead:     { type: Boolean, default: false }
+    recipient: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: Schema.Types.ObjectId, ref: "User" },
+    type: { type: String, enum: ["NEW_BLOG", "LIKE", "NEW_COMMENT", "LIKE_COMMENT", "FOLLOW_REQUEST"], required: true },
+    message: { type: String, required: true },
+    blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
+    status: { type: String, enum: ["PENDING", "ACCEPTED", "REJECTED", "READ"], default: "PENDING" },
+    isRead: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
